3d6812b20a120aed0c4f738ef84451fa93ca752c
Faruk Acibal <acfaruk@gmail.com>
2018-04-02 11:21:08 +0200

    better spawning for meteors

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,8 +6,12 @@ var meteor_scene = preload("res://Scenes/Meteor.tscn")
 export (float) var turn_amount = 0.05
 export (float) var max_speed = 4
 export (float) var acceleration = 0.1
+export (int) var max_meteor_distance_squared = 2000*2000
+export (int) var min_meteor_distance = 1050
+export (int) var max_meteors = 20
 
 var cur_speed = 0
+var meteors = []
 
 func _ready():
 	pass
@@ -34,6 +38,7 @@ func _physics_process(delta):
 		
 	move_local_y(-cur_speed)
 	
+	despawn_meteors()
 	spawn_meteors()
 	
 func shoot():
@@ -43,15 +48,21 @@ func shoot():
 	bullet.move_local_y(-25)
 	get_node("/root/Node").add_child(bullet)
 	
+func despawn_meteors():
+	for meteor in meteors:
+		if meteor.position.distance_squared_to($main_cam.get_camera_position()) > max_meteor_distance_squared:
+			meteor.queue_free()
+			meteors.erase(meteor)
+
 func spawn_meteors():
-	var meteor = meteor_scene.instance()
-	
-	var min_distance = 1050
-	var dist = randi() % 200 + min_distance
-	var rot = randi() % 360
-	
-	meteor.position = $main_cam.position
-	meteor.rotation_degrees = rot
-	meteor.move_local_y(-dist)
-	print(meteor.position)
-	get_node("/root/Node").add_child(meteor)
+	if meteors.size() < max_meteors:
+		var meteor = meteor_scene.instance()
+		meteors.append(meteor)
+		
+		var dist = randi() % 200 + min_meteor_distance
+		var rot = randi() % 360
+		
+		meteor.position = $main_cam.get_camera_position()
+		meteor.rotation_degrees = rot
+		meteor.move_local_y(-dist)
+		get_node("/root/Node").add_child(meteor)
