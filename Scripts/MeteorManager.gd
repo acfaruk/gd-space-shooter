bbe86454e10f1c13787688d4438b593eead6c2ec
Faruk Acibal <acfaruk@gmail.com>
2018-04-05 22:17:37 +0200

    remove last get_node call and use find_node instead on init

--- Scripts/MeteorManager.gd
+++ Scripts/MeteorManager.gd
@@ -4,18 +4,16 @@ extends Node
 const meteor_scene = preload("res://Scenes/meteors/big_meteor.tscn")
 
 #EXPORT
-export (NodePath) var world_path
-export (NodePath) var player_path
 export (int) var max_meteor_distance_squared = 2000*2000
 export (int) var min_meteor_distance = 1050
 export (int) var max_meteors = 30
 
-#NODES
-onready var world = get_node(world_path)
-onready var player = get_node(player_path)
-
 #VARS
 var meteors = []
+var player
+
+func _ready():
+	player = get_parent().find_node("player")
 
 func _physics_process(delta):
 	despawn_meteors()
@@ -31,7 +29,7 @@ func spawn_meteors():
 		var meteor = meteor_scene.instance()
 		var distance = randi() % 200 + min_meteor_distance
 		meteor.setup(player.position, self, distance)
-		world.add_child(meteor)
+		get_parent().add_child(meteor)
 		
 func remove_meteor(meteor):
 	meteors.erase(meteor)
